c6f0a82fef6d1dfc5ba40131b7545757292d0dc8
Aydogan Ersoz <aydoganersoz@protonmail.com>
2020-03-27 08:53:38 +0100

    session pattern is functional

--- src/tomat.rs
+++ src/tomat.rs
@@ -1,9 +1,27 @@
 use crate::args;
 use crate::timer;
+use chrono::Local;
 
 pub fn run_tomat(durations: args::Durations) {
-  println!("tomat is running {}", durations.pomodoro);
+  let sessions = [
+    (durations.pomodoro, "pomodoro"),
+    (durations.short_break, "short break"),
+    (durations.pomodoro, "pomodoro"),
+    (durations.short_break, "short break"),
+    (durations.pomodoro, "pomodoro"),
+    (durations.long_break, "long break"),
+  ];
 
-  timer::start_timer(durations.pomodoro);
-  println!("timer run terminated");
+  loop {
+    for session in sessions.iter() {
+      let now = Local::now().format("%H:%M:%S");
+      println!(
+        "{} {} started for {} minutes",
+        now,
+        (*session).1,
+        (*session).0
+      );
+      timer::start_timer((*session).0);
+    }
+  }
 }
