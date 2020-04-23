05e1f10870d7f0fb01f3a2fb872332109527dc6c
Aydogan Ersoz <aydoganersoz@protonmail.com>
2020-04-23 08:43:53 +0200

    reset feature is functional

--- src/storage.rs
+++ src/storage.rs
@@ -41,19 +41,20 @@ pub fn show_db() -> Result<Vec<Stats>> {
   let conn = Connection::open(DB_PATH)?;
 
   let mut stmt = conn.prepare(
-    "select
-    sum(session_kind= 0) session_kind_0,
-    sum(session_kind= 1) session_kind_1,
-    sum(session_kind= 2) session_kind_2,
-    date(ts) ts_day
-from stats
-group by date(ts)
-order by ts_day desc",
+    "SELECT
+      SUM(session_kind = 0) session_kind_0,
+      SUM(session_kind = 1) session_kind_1,
+      SUM(session_kind = 2) session_kind_2,
+      DATE(ts) ts_day
+    FROM stats
+    GROUP BY DATE(ts)
+    ORDER BY ts_day DESC",
   )?;
-  let mut names = Vec::<Stats>::new();
+
+  let mut stat = Vec::<Stats>::new();
   let mut rows = stmt.query(NO_PARAMS)?;
   while let Some(row) = rows.next()? {
-    names.push(Stats {
+    stat.push(Stats {
       completed_pomodoro: row.get(0)?,
       completed_short_break: row.get(1)?,
       completed_long_break: row.get(2)?,
@@ -61,5 +62,13 @@ order by ts_day desc",
     });
   }
 
-  Ok(names)
+  Ok(stat)
+}
+
+pub fn reset_db() -> Result<()> {
+  let conn = Connection::open(DB_PATH)?;
+
+  conn.execute("DELETE FROM stats", NO_PARAMS)?;
+
+  Ok(())
 }
